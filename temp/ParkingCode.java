c85b222ec549a5df0baaba9fc439131d4dd31bfc
BrunoHernandez12 <32974591+BrunoHernandez14@users.noreply.github.com>
2019-12-14 14:47:40 -0600

    Update ParkingCode.java

--- temp/ParkingCode.java
+++ temp/ParkingCode.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@Autonomous(name = "ParkingAuto")
 public class ParkingCode extends LinearOpMode {
 
     public DcMotor frontLeft;
